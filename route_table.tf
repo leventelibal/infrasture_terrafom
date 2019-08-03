710c0d31a30f4c82644251949ffd2127d790be4d
leventelibal <leventelibal@gmail.com>
2019-08-03 12:53:06 -0400

    add route table

---
+++ route_table.tf
@@ -0,0 +1,21 @@
+resource "aws_route_table" "r" {
+  vpc_id = "${aws_vpc.dev.id}"
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  #route {
+  #  ipv6_cidr_block        = "::/0"
+  #  egress_only_gateway_id = "${aws_egress_only_internet_gateway.foo.id}"
+  #}
+
+   tags = {
+      Name       = "${var.Name}"
+      Env        = "${var.Env}"
+      Created_by = "${var.Created_by}"
+      Dept       = "${var.Dept}"
+  }
+  }
+}
