056d087f2cea91726a535881759f14d38bdc3820
leventelibal <leventelibal@gmail.com>
2019-08-03 12:09:30 -0400

    { add

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,11 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-}
+
 
 tags = {
     Name = "${var.Name}"
     Env = "${var.Env}"
     Created_by = "${var.Created_by}"
     Dept = "${var.Dept}"
+}
 }
