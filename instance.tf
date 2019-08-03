35cde99197c7f64d26287ab800ed446c20db3fbc
leventelibal <leventelibal@gmail.com>
2019-08-03 13:41:13 -0400

    add the sunbet

--- instance.tf
+++ instance.tf
@@ -2,8 +2,7 @@ resource "aws_instance" "web1" {
     ami = "${var.ami}"
     instance_type = "${var.instance_type}"
     key_name = "${var.key_name}"
-    subnet_id = ""
-    security_groups = ""
+    subnet_id = "${aws_security_group.public.id}"
     vpc_security_group_ids = ["${aws_security_group.public.id}"]
     associate_public_ip_address = "true"
     user_data = "${file("user_data.sh")}"
