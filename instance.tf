744528c31e0257b7a4e83ab028c1a8809769a346
leventelibal <leventelibal@gmail.com>
2019-08-03 13:43:22 -0400

    change correct the subnet

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web1" {
     ami = "${var.ami}"
     instance_type = "${var.instance_type}"
     key_name = "${var.key_name}"
-    subnet_id = "${aws_security_group.public.id}"
+    subnet_id = "${aws_subnet.public.id}"
     vpc_security_group_ids = ["${aws_security_group.public.id}"]
     associate_public_ip_address = "true"
     user_data = "${file("user_data.sh")}"
