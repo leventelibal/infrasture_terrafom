9922e1f2d9626d56c9559b98248f63c22e4c6b7e
leventelibal <leventelibal@gmail.com>
2019-08-03 12:30:52 -0400

    p

--- subnet.tf
+++ subnet.tf
@@ -1,8 +1,8 @@
-resource "aws_subnet" "Public" {
+resource "aws_subnet" "public" {
   vpc_id     = "${aws_vpc.dev.id}"
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "Public"
+    Name = "public"
   }
 }
