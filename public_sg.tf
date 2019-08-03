6cbf341aad4893c5d56e5035131cb66fc664a9b7
leventelibal <leventelibal@gmail.com>
2019-08-03 13:05:07 -0400

    []changes

--- public_sg.tf
+++ public_sg.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "public" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = "[0.0.0.0/0]"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
     ingress {
@@ -16,14 +16,14 @@ resource "aws_security_group" "public" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = "[0.0.0.0/0]"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
    
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = "[0.0.0.0/0]"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port       = 0
