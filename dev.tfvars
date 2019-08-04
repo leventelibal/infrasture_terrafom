669466877b5a8acebb16167be2c3697bb72023aa
leventelibal <leventelibal@gmail.com>
2019-08-04 18:02:10 -0400

    add var in

--- dev.tfvars
+++ dev.tfvars
@@ -8,4 +8,10 @@ key_name   = "terraform-key"
 user_data  = "userdata.sh"
 region     ="us-east-1"
 appname    ="wordpress"
-    
+min_app_size   = 6
+max_app_size   = 128
+desired_app_capacity  = 6    
+
+min_db_size   = 6
+max_db_size   = 128
+desired_db_capacity  = 6   
--- variable.tf
+++ variable.tf
@@ -7,3 +7,18 @@ variable "instance_type" {}
 variable "key_name" {}
 variable "region" {}
 variable "appname" {}
+variable "appname" {}
+variable "min_size" {}
+variable "max_size" {}
+variable "desired_db_capacity" {}
+variable "min_db-size" {}
+variable "max_db-size" {}
+variable "desired_app_capacity" {}
+variable "min_app-size" {}
+variable "max_app-size" {}
+
+  
+}
+
+
+
