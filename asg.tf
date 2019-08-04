2533aa1803e3285b19463365d75e6292e4b22162
leventelibal <leventelibal@gmail.com>
2019-08-04 16:59:47 -0400

    tags as map changes

--- asg.tf
+++ asg.tf
@@ -19,10 +19,10 @@ module "wordpress" {
   desired_capacity          = 4
   wait_for_capacity_timeout = 0
 
-tags = {
+tags_as_map = {
     Name = "${var.Name}"
     Env = "${var.Env}"
     Created_by = "${var.Created_by}"
     Dept = "${var.Dept}
-}
+ }
 }
