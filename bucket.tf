80d8324a1f886a384e25fbc23b55db3e485c2fd2
leventelibal <leventelibal@gmail.com>
2019-08-04 15:53:05 -0400

    change the vpc and subnet

--- bucket.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3" "bucketname" {
-    bucket =   "infrasructure-levent-april-2019"
-  
-}
--- dont_create.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3" "bucketnameb" {
-    bucket =   "infrasructure-levent-2019b"
-  
-}
